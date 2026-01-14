27b2d41345d58d822fc7b4b2adfa3e413292e152
Kore Sampath Kumar <sampathkore4444@gmail.comr>
2026-01-14 14:36:35 +0700

    Initial OCR Streamlit app

--- app.py
+++ app.py
@@ -5,9 +5,7 @@ import numpy as np
 from PIL import Image
 
 st.set_page_config(page_title="OCR Confidence App", layout="wide")
-st.title(
-    "📄 Smart OCR: Multiple Languages Text Extraction with Confidence Visualization"
-)
+st.title("📄 Smart OCR: Multi-Languages Text Extraction with Confidence Visualization")
 st.write("Upload an image, extract text, and see confidence for each word")
 
 # -------------------------------
